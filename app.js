6952183a276bd223a60ec4cf62dc79a310f7639b
patricia-benites <patricia.benites.cava@gmail.com>
2022-01-24 20:55:25 +0100

    add iteration 4

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
+
+hbs.registerPartials(__dirname + '/views/partials');
+
 // setting the spotify-api goes here:
 
 const spotifyApi = new SpotifyWebApi({
@@ -37,11 +40,30 @@ app.get('/artist-search', (req, res, next) => {
         .searchArtists(req.query.search)
         .then(data => {
             console.log('The received data from the API: ', data.body.artists.items[0].images[0].url);
-            res.render("search", { query: req.query.search, artists: data.body.artists.items });
+            res.render("artist-search-result", { query: req.query.search, artists: data.body.artists.items });
         })
         .catch(err => console.log('The error while searching artists occurred: ', err));
 
 });
 
+app.get('/albums/:artistId', (req, res, next) => {
+    spotifyApi
+        .getArtistAlbums(req.params.artistId)
+        .then(data => {
+            console.log('The received data from the API: ', data.body);
+            res.render("albums", { albums: data.body.items });
+        })
+        .catch(err => console.log('The error while searching artists occurred: ', err));
+});
 
+
+app.get('/tracks/:albumId', (req, res, next) => {
+    spotifyApi
+        .getAlbumTracks(req.params.albumId)
+        .then(data => {
+            console.log('The received data from the API: ', data.body);
+            res.render("tracks", { tracks: data.body.items });
+        })
+        .catch(err => console.log('The error while searching artists occurred: ', err));
+});
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
